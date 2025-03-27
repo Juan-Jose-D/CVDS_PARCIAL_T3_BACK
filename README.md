b27995d884d1c6f2c290a5f5857a9d77e1c7fde0
unknown <juan.dgomez@labinfo.is.escuelaing.edu.co>
2025-03-27 14:33:11 -0500

    Back funcional, se implementó CORS en la app para evitar futuros errores

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Nuestro modelo será así, tendremos una clase Product y una clase Payment.
 
 ![Model](image.png)
 
+Una vez tenemos nuestro BACK funcionando vamos a crear unos productos manualmente ya que en nuestro front no tendremos lugar para crear productos, esto iría en una vista propioa de administrador pero se escaparía del tiempo que tenemos para este examen.
 
+![Productos](image-1.png)
--- src/main/java/edu/eci/cvds/parcial/ParcialApplication.java
+++ src/main/java/edu/eci/cvds/parcial/ParcialApplication.java
@@ -2,6 +2,10 @@ package edu.eci.cvds.parcial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ParcialApplication {
@@ -9,5 +13,22 @@ public class ParcialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParcialApplication.class, args);
 	}
+
+	@Configuration
+     public static class CorsConfig {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "http://localhost:5500")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+				}
+			};
+		}
+}
 
 }
